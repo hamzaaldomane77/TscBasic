6490e45e5942b97ec9cb8052ca49c7ac8c709935
Hamza <hamzaaldomane@gmail.com>
2024-08-28 09:24:52 +0300

    first commit

---
+++ index.ts
@@ -0,0 +1,18 @@
+const fullname:string ="hamza";
+const age:number|string ="22";
+const currenLocation:string="Damascus";
+const office1:Array<string|Number> =[
+"zahers" , "shnaea" , 22
+];
+const office2:(string | number | Array<string>) []=[
+    "zahers" , "shnaea" ,12
+    ];
+const isInDamascus:boolean = currenLocation =="Damascus";
+ const getSalare : () =>string =()=>{
+    return "1000.100";
+ };
+
+ const spasfetio : {unv:string , SVA:String} ={
+    unv : "JPU",
+    SVA:"ITE",
+ }
